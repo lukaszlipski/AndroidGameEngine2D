f7ceef97e6b9b3f45f317fe61ad8ed9ac57ec1e9
lukaszlipski <luc111807@gmail.com>
2016-10-11 23:09:43 +0200

    First test commit

---
+++ app/src/main/java/com/lucek/androidgameengine2d/graphics/Window.java
@@ -0,0 +1,15 @@
+package com.lucek.androidgameengine2d.graphics;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.lucek.androidgameengine2d.R;
+
+public class Window extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
