22f050cb11befc463a15dc3949327c11ec337da6
Alan Kaliński <alankalinski@gmail.com>
2016-11-26 13:03:27 +0100

    Shared Prefs added

---
+++ app/src/main/java/com/lucek/androidgameengine2d/storage/PreferencesManager.java
@@ -0,0 +1,57 @@
+package com.lucek.androidgameengine2d.storage;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import com.lucek.androidgameengine2d.GeApplication;
+
+public class PreferencesManager {
+    private static final SharedPreferences getter;
+    private static final SharedPreferences.Editor setter;
+
+    static {
+        Context context = GeApplication.getAppContext();
+        getter = context.getSharedPreferences("gameengine_preferences", Context.MODE_PRIVATE);
+        setter = getter.edit();
+    }
+
+    private static void clear(String... keys) {
+        for (String key : keys)
+            setter.remove(key);
+        setter.apply();
+    }
+
+    private static void set(String key, String value) {
+        setter.putString(key, value);
+        setter.apply();
+    }
+
+    private static void set(String key, Boolean value) {
+        setter.putBoolean(key, value);
+        setter.apply();
+    }
+
+    @Nullable
+    private static String get(@NonNull String key, @Nullable String defaultVaule) {
+        return getter.getString(key, defaultVaule);
+    }
+
+    @Nullable
+    private static Boolean get(@NonNull String key, @Nullable Boolean defaultVaule) {
+        return getter.getBoolean(key, defaultVaule == null ? false : defaultVaule);
+    }
+
+    public static class NoGo {
+        private static final String ALGORITHM_TOURNAMENT = "algorithm_tournament";
+
+        public static Boolean getAlgorithmTournament() {
+            return get(ALGORITHM_TOURNAMENT, false);
+        }
+
+        public static void setAlgorithmTournament(Boolean value) {
+            set(ALGORITHM_TOURNAMENT, value);
+        }
+    }
+}
